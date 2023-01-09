7ca0ec4779eabf2fce110360800e740c05f06315
OiAlisson <103596988+OiAlisson@users.noreply.github.com>
2023-01-09 12:31:38 -0300

    Opção sair do sistema
    Incluso no sistema a opção de saída para o usuário.

--- Proj111C#.cpp
+++ Proj111C#.cpp
@@ -124,6 +124,7 @@ int main()
 	   printf("\t1 - Registrar nomes\n");
 	   printf("\t2 - Consultar nomes\n");
 	   printf("\t3 - Deletar nomes\n\n");
+	   printf("\t4 - Sair do sistema\n\n");
 	   printf("Escolha sua opção: "); //Final do menu
 	
 	   scanf("%d", &opcao); //Aremazenando a escolha do usuário
@@ -142,6 +143,11 @@ int main()
 		  
 		  case 3:
           deletar();//chamada de funções
+	  	  break;
+	  	  
+	  	  case 4:
+	  	  printf("Obrigado por utilizar o sistema!\n");
+	  	  return 0;
 	  	  break;
 	  	  
 	  	  default:
